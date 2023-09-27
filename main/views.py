df401312e11bd96672fb0251010945ba65bad308
adipppp <adhif2004@gmail.com>
2023-09-27 10:49:04 +0700

    Commit Tutorial 3

--- main/views.py
+++ main/views.py
@@ -1,44 +1,84 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+import datetime
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from main.models import Product
 from django.core import serializers
 
 # Create your views here.
-def show_main(request):
-    products = Product.objects.all()
-    
+@login_required(login_url='/login')
+def show_main(request: HttpRequest):
+    products = Product.objects.filter(user=request.user)
+
     context = {
-        'name': 'Fernanda Nadhiftya Putra',
+        'name': request.user.username,
         'class': 'PBP D',
-        'products': products
+        'products': products,
+        'last_login': request.COOKIES['last_login']
     }
 
     return render(request, "main.html", context)
 
-def create_product(request):
+def create_product(request: HttpRequest):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
     return render(request, "create_product.html", context)
 
-def show_xml(request):
+def show_xml(request: HttpRequest):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
-def show_json(request):
+def show_json(request: HttpRequest):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_xml_by_id(request, id):
+def show_xml_by_id(request: HttpRequest, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
-def show_json_by_id(request, id):
+def show_json_by_id(request: HttpRequest, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request: HttpRequest):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request: HttpRequest):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request: HttpRequest):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
